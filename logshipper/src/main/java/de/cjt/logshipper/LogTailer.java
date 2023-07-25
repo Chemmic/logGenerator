4e824a492208ee110ee5daa71b26165e121796d5
Matteo Staar <matteo.staar@cjt.de>
2023-07-25 13:21:49 +0200

    sq

--- logshipper/src/main/java/de/cjt/logshipper/LogTailer.java
+++ logshipper/src/main/java/de/cjt/logshipper/LogTailer.java
@@ -4,12 +4,17 @@ import java.io.File;
 
 import org.apache.commons.io.input.Tailer;
 import org.apache.commons.io.input.TailerListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import de.cjt.controller.LoggingController;
+
 @Component
 public class LogTailer {
+	Logger logger = LoggerFactory.getLogger(LoggingController.class);
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void doSomethingAfterStartup() {
@@ -49,8 +54,30 @@ public class LogTailer {
 			}
 		};
 		//Tailer t = new Tailer(new File("C:/Matteo/Workspaces/Logshipper/logshipper/logs/rolling.log"), tl);
-		Tailer t = new Tailer(new File("C:/Matteo/Workspaces/Logshipper/logshipper/logs/rolling.log"), tl, 1l, true, false);
-		t.run();
+	//	Tailer t = new Tailer(new File("C:/Matteo/Workspaces/Logshipper/logshipper/logs/rolling.log"), tl, 1l, true, false);
+	//	t.run();
+		Thread t = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while(true) {
+					try {
+						Thread.sleep(1000);
+				        logger.trace("A TRACE Message");
+				        logger.debug("A DEBUG Message");
+				        logger.info("An INFO Message");
+				        logger.warn("A WARN Message");
+				        logger.error("An ERROR Message");
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+			}
+		});
+		t.start();
 	}
 	
 }
